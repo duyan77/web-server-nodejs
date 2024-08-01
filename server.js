9081545c82da1f549349988c1a594d4a93686d8d
duyan77 <fklt0961@gmail.com>
2024-08-01 11:08:01 +0700

    format project

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-// const router = require("./apiRouter.js");
-
-// app.set("view engine", "ejs"); // setting the view engine to ejs
-
-// // app.get("/", (req, res) => {
-// //   res.render("index", { text: "Hello World!" }); // rendering the index.ejs file
-// // });
-
-// app.use(express.static("public")); // using the public folder
-
-// app.use("/router", router); // using the router
-
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
-// const path = require("path");
-
-// ==============================================
 const express = require("express");
 const path = require("path");
 const servers = require("./servers.json").servers;
@@ -27,7 +6,7 @@ servers.forEach((server) => {
   const site = express();
 
   // Cấu hình để phục vụ các tệp tĩnh
-  // site.use(express.static(path.join(__dirname, "public")));
+  site.use(express.static(path.join(__dirname, "public")));
 
   // Lắng nghe trên cổng tương ứng với từng site
   site.listen(server.port, () => {
